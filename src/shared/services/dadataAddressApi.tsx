7c52f5d838b242d8f18dadbdb2232db37fa6fd02
migger <migger@yandex.ru>
2018-06-05 11:04:50 +0300

    Fixed critical issues

--- src/shared/services/dadataAddressApi.tsx
+++ src/shared/services/dadataAddressApi.tsx
@@ -1,10 +1,14 @@
 import Axios from 'axios';
 
+class DaDataResponse {
+    public value: string;
+}
+
 export class DadataAddressApi {
 
-    public getSuggestions = (input, callback) => {
+    public getSuggestions = (input: string, callback: Function) => {
         Axios.post('https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address', {query: input}, {headers: {
                 Authorization: 'Token 4aa524ecd2cdc4bd2c036d8a4cb51a125b09e3c0',
-            }}).then((r) => callback(r.data.suggestions.map(item => item.value)));
+            }}).then((r) => callback(r.data.suggestions.map((item: DaDataResponse) => item.value)));
     }
 }
